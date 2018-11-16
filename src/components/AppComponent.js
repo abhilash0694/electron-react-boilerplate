5af9c29083129d51142f7aacc88674c915cd37a1
Abhilash Pandey <abhilash.pandey@reverieinc.com>
2018-11-16 11:57:39 +0530

    Initial commit

---
+++ src/components/AppComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <article>
+                <h1>My First Electron-React App.</h1>
+            </article>
+        )
+    }
+}
